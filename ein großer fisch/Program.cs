7d6cafca991bc0b5639e522a8f28fa823cf8cc1b
Lewis Schofield <22LSchofield@heckgrammar.co.uk>
2026-01-30 12:19:29 +0000

    Add project files.

---
+++ ein großer fisch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ein_großer_fisch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter height of tank(m):");
+            double Tank_height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter width of tank(m):");
+            double Tank_width = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter depth of tank(m):");
+            double Tank_depth = Convert.ToDouble(Console.ReadLine());
+
+            double Tank_volume = Building_The_Tank_Volume(Tank_height, Tank_width, Tank_depth);
+            Console.WriteLine("Volume of tank(m³)=" + Tank_volume);
+
+            double Water_volume_metres = Building_The_Tank_Volume_Water(Tank_volume);// vol of water in m^3
+            double Water_volume_litres = Water_volume_metres * 1000; // vol of water in litres
+            Console.WriteLine("Recommended volume of water(m³)=" + Water_volume_metres);
+            Console.WriteLine("Recommended volume of water(l)=" + Water_volume_litres);
+        }
+
+        static double Building_The_Tank_Volume(double height, double width, double depth) // calculates vol of tank
+        {
+            double volume = height * width * depth;
+            return volume;
+        }
+        static double Building_The_Tank_Volume_Water(double TankVolume) // calculates amount of water that can go in tank
+        {
+            double Water_volume = TankVolume * 0.8; // vol of water in m^3
+            return Water_volume;
+        }
+        static void Building_The_Tank_Volume_Glass_Thickness()
+        {
+
+        }
+    }
+}
+
---
+++ ein großer fisch/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ein großer fisch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HGS-SCCM")]
+[assembly: AssemblyProduct("ein großer fisch")]
+[assembly: AssemblyCopyright("Copyright © HGS-SCCM 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("22c3dc70-00c0-441f-a1d9-c8011e4a7219")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
